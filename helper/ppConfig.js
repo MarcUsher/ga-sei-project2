7ab231798cff33d5b9466397d5480316ba38607e
rakitent <myphd1@yahoo.co.uk>
2022-06-08 09:44:12 +0100

    installation of initial passport authentication packages

---
+++ helper/ppConfig.js
@@ -0,0 +1,14 @@
+const passport = require('passport')
+const LocalStrategy = require('passport-local').Strategy
+// copied from passportjs.org
+passport.use(new LocalStrategy(
+    function(username, password, done) {
+      User.findOne({ username: username }, function (err, user) {
+        if (err) { return done(err); }
+        if (!user) { return done(null, false); }
+        if (!user.verifyPassword(password)) { return done(null, false); }
+        return done(null, user);
+      });
+    }
+  ));
+  
